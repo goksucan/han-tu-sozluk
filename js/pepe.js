3db92049542b5e8cc64997914f2488bef2ef323d
goksucan <30368027+goksucan@users.noreply.github.com>
2017-10-20 11:15:27 +0300

    Initial commit

---
+++ js/pepe.js
@@ -0,0 +1,10 @@
+  function play(){
+       var audio = document.getElementById("audio");
+       audio.play();
+                 }
+
+
+  function play(){
+       var audio = document.getElementById("audio1");
+       audio.play();
+                 }
